1b7b0ec8b5d77ffdb29619ac716d58b21c9fd3b0
msamgan <mohdsamgankhan@gmail.com>
2022-07-25 16:20:42 +0400

    Formatted.

---
+++ utils/commands.js
@@ -0,0 +1,123 @@
+const vscode = require("vscode")
+const cp = require("child_process")
+const { checkBinaryExist, infoMessage, projectDirectory, errorMessage, currentEditor, copyPintJson } = require("./methods")
+const { PINT_BINARY } = require("./constants")
+
+/**
+ * Runs the Laravel Pint command on the current file.       
+ * @returns None       
+ */
+exports.formatCurrentFile = async () => {
+    if (!checkBinaryExist()) {
+        return await vscode.window
+            .showInformationMessage("Pint binary not found. Do you want in install?", "Yes", "No")
+            .then((answer) => {
+                infoMessage("Initiating installation...")
+                if (answer === "Yes") {
+                    cp.exec(
+                        "composer require laravel/pint",
+                        {
+                            cwd: projectDirectory()
+                        },
+                        // eslint-disable-next-line no-unused-vars
+                        (err, stdout, stderr) => {
+                            if (err) {
+                                return errorMessage("Something went wrong while running Laravel Pint.")
+                            } else {
+                                return infoMessage("Initiating complete...")
+                            }
+                        }
+                    )
+                }
+            })
+    }
+
+    cp.exec(
+        PINT_BINARY + " " + currentEditor(),
+        {
+            cwd: projectDirectory()
+        },
+        // eslint-disable-next-line no-unused-vars
+        (err, stdout, stderr) => {
+            if (err) {
+                return errorMessage("Something went wrong while running Laravel Pint.")
+            } else {
+                return infoMessage("Formatting your current file with Laravel Pint.")
+            }
+        }
+    )
+}
+
+/**
+ * Runs the Laravel Pint command to format the project.           
+ * @returns None           
+ */
+exports.formatProject = async () => {
+    if (!checkBinaryExist()) {
+        return await vscode.window
+            .showInformationMessage("Pint binary not found. Do you want in install?", "Yes", "No")
+            .then((answer) => {
+                infoMessage("Initiating installation...")
+                if (answer === "Yes") {
+                    cp.exec(
+                        "composer require laravel/pint",
+                        {
+                            cwd: projectDirectory()
+                        },
+                        // eslint-disable-next-line no-unused-vars
+                        (err, stdout, stderr) => {
+                            if (err) {
+                                return errorMessage("Something went wrong while running Laravel Pint.")
+                            } else {
+                                return infoMessage("Initiating complete...")
+                            }
+                        }
+                    )
+                }
+            })
+    }
+
+    cp.exec(
+        PINT_BINARY,
+        {
+            cwd: projectDirectory()
+        },
+        // eslint-disable-next-line no-unused-vars
+        (err, stdout, stderr) => {
+            if (err) {
+                return errorMessage("Something went wrong while running Laravel Pint.")
+            } else {
+                return infoMessage("Formatting your project with Laravel Pint.")
+            }
+        }
+    )
+}
+
+exports.copyPintJsonToRoot = async () => {
+    if (!checkBinaryExist()) {
+        return await vscode.window
+            .showInformationMessage("Pint binary not found. Do you want in install?", "Yes", "No")
+            .then((answer) => {
+                infoMessage("Initiating installation...")
+                if (answer === "Yes") {
+                    cp.exec(
+                        "composer require laravel/pint",
+                        {
+                            cwd: projectDirectory()
+                        },
+                        // eslint-disable-next-line no-unused-vars
+                        (err, stdout, stderr) => {
+                            if (err) {
+                                return errorMessage("Something went wrong while running Laravel Pint.")
+                            } else {
+                                return infoMessage("Initiating complete...")
+                            }
+                        }
+                    )
+                }
+            })
+    }
+
+    copyPintJson()
+    return infoMessage("pint.json copied to your project.")
+}
