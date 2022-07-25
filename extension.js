6d166dc07c7f784864da32af4e9a5df0e9fa5778
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2022-07-25 16:54:19 +0400

    V0.1.0 (#15)
    * Format on save (#14)
    
    * v0.1.0

--- extension.js
+++ extension.js
@@ -1,135 +1,42 @@
 const vscode = require("vscode")
-const cp = require("child_process")
-const {
-    checkBinaryExist,
-    projectDirectory,
-    infoMessage,
-    errorMessage,
-    currentEditor,
-    copyPintJson
-} = require("./utils/methods")
-const { PINT_BINARY } = require("./utils/constants")
+const { formatCurrentFile, formatProject, copyPintJsonToRoot } = require("./utils/commands")
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let format = vscode.commands.registerCommand("laravel-pint-vscode.format", async () => {
-        if (!checkBinaryExist()) {
-            return await vscode.window
-                .showInformationMessage("Pint binary not found. Do you want in install?", "Yes", "No")
-                .then((answer) => {
-                    infoMessage("Initiating installation...")
-                    if (answer === "Yes") {
-                        cp.exec(
-                            "composer require laravel/pint",
-                            {
-                                cwd: projectDirectory()
-                            },
-                            // eslint-disable-next-line no-unused-vars
-                            (err, stdout, stderr) => {
-                                if (err) {
-                                    return errorMessage("Something went wrong while running Laravel Pint.")
-                                } else {
-                                    return infoMessage("Initiating complete...")
-                                }
-                            }
-                        )
-                    }
-                })
-        }
+    context.subscriptions.push(
+        vscode.commands.registerCommand("laravel-pint-vscode.format", async () => {
+            await formatProject()
+        })
+    )
 
-        cp.exec(
-            PINT_BINARY,
-            {
-                cwd: projectDirectory()
-            },
-            // eslint-disable-next-line no-unused-vars
-            (err, stdout, stderr) => {
-                if (err) {
-                    return errorMessage("Something went wrong while running Laravel Pint.")
-                } else {
-                    return infoMessage("Formatting your project with Laravel Pint.")
+    context.subscriptions.push(
+        vscode.commands.registerCommand("laravel-pint-vscode.format-file", async () => {
+            await formatCurrentFile()
+        })
+    )
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand("laravel-pint-vscode.publish-config", async () => {
+            copyPintJsonToRoot()
+        })
+    )
+
+    context.subscriptions.push(
+        vscode.workspace.onWillSaveTextDocument(async (event) => {
+            if (event.document.fileName.endsWith(".php")) {
+                let allConfig = JSON.parse(JSON.stringify(await vscode.workspace.getConfiguration()))
+                try {
+                    if (allConfig.editor.laravel.pint.enabled) {
+                        await formatCurrentFile(false)
+                    }
+                } catch (e) {
+                    // console.log(e)
                 }
             }
-        )
-    })
-
-    context.subscriptions.push(format)
-
-    let formatFile = vscode.commands.registerCommand("laravel-pint-vscode.format-file", async () => {
-        if (!checkBinaryExist()) {
-            return await vscode.window
-                .showInformationMessage("Pint binary not found. Do you want in install?", "Yes", "No")
-                .then((answer) => {
-                    infoMessage("Initiating installation...")
-                    if (answer === "Yes") {
-                        cp.exec(
-                            "composer require laravel/pint",
-                            {
-                                cwd: projectDirectory()
-                            },
-                            // eslint-disable-next-line no-unused-vars
-                            (err, stdout, stderr) => {
-                                if (err) {
-                                    return errorMessage("Something went wrong while running Laravel Pint.")
-                                } else {
-                                    return infoMessage("Initiating complete...")
-                                }
-                            }
-                        )
-                    }
-                })
-        }
-
-        cp.exec(
-            PINT_BINARY + " " + currentEditor(),
-            {
-                cwd: projectDirectory()
-            },
-            // eslint-disable-next-line no-unused-vars
-            (err, stdout, stderr) => {
-                if (err) {
-                    return errorMessage("Something went wrong while running Laravel Pint.")
-                } else {
-                    return infoMessage("Formatting your current file with Laravel Pint.")
-                }
-            }
-        )
-    })
-
-    context.subscriptions.push(formatFile)
-
-    let publishConfig = vscode.commands.registerCommand("laravel-pint-vscode.publish-config", async () => {
-        if (!checkBinaryExist()) {
-            return await vscode.window
-                .showInformationMessage("Pint binary not found. Do you want in install?", "Yes", "No")
-                .then((answer) => {
-                    infoMessage("Initiating installation...")
-                    if (answer === "Yes") {
-                        cp.exec(
-                            "composer require laravel/pint",
-                            {
-                                cwd: projectDirectory()
-                            },
-                            // eslint-disable-next-line no-unused-vars
-                            (err, stdout, stderr) => {
-                                if (err) {
-                                    return errorMessage("Something went wrong while running Laravel Pint.")
-                                } else {
-                                    return infoMessage("Initiating complete...")
-                                }
-                            }
-                        )
-                    }
-                })
-        }
-
-        copyPintJson()
-        return infoMessage("pint.json copied to your project.")
-    })
-
-    context.subscriptions.push(publishConfig)
+        })
+    )
 }
 
 // this method is called when your extension is deactivated
